4521cccb89ad5aa3673f23d53acbaf5e459ca7bc
gamecodechampion <stupitmems@gmail.com>
2025-01-03 01:34:25 -0600

    Got program to check if it is started or not, also started checking for key input.

--- AfkFishing/Program.cs
+++ AfkFishing/Program.cs
@@ -1,12 +1,31 @@
 ﻿using System.Diagnostics;
+using System.Runtime.InteropServices;
+using Microsoft.Win32;
 
 class Program
 {
+    [DllImport("user32.dll")]
+
+    public static extern int GetAsyncKeyState(Int32 i);
+
+    private int KEY1 = 46;
+    private int KEY2 = 44;
     static void Main(string[] args)
     {
-        if (!IsProgamRunning()) 
+        string command = $@"/C E:\Steam\steam.exe -applaunch 3146520";
+
+
+        if (!IsProgamRunning())
         {
-            
+            MessageBox.Show(
+                "Webfishing is not running! The program will launch the game for you now. Restart this program once webfishing starts!",
+                String.Empty, MessageBoxButtons.OK, MessageBoxIcon.Information);
+            StartGame(command);
+        }
+
+        while (IsProgamRunning())
+        {
+            Console.WriteLine("yeah");
         }
     }
 
@@ -14,4 +33,20 @@ class Program
     {
         return Process.GetProcessesByName("webfishing").Length > 0;
     }
+
+    static Process StartGame(string command)
+    {
+        return Process.Start("CMD.exe", command);
+    }
+
+    static void Keybind()
+    {
+        while (true)
+        {
+            // Thread.Sleep(5);
+            // int keyState1 = GetAsyncKeyState(KEY1);
+        }
+    }
+    
+
 }
