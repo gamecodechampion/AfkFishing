2adf0739b956dade93aafcb54ceef1acfa1b6b2a
gamecodechampion <stupitmems@gmail.com>
2025-01-03 00:45:04 -0600

    Checking if the process exists

--- AfkFishing/Program.cs
+++ AfkFishing/Program.cs
@@ -1,7 +1,17 @@
-﻿class Program
+﻿using System.Diagnostics;
+
+class Program
 {
     static void Main(string[] args)
     {
-        
+        if (!IsProgamRunning()) 
+        {
+            
+        }
+    }
+
+    static bool IsProgamRunning()
+    {
+        return Process.GetProcessesByName("webfishing").Length > 0;
     }
 }
